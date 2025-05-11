8a36198f14061bbe123e09414d7d2223f89d4f00
Stil-sh <denios228s@gmail.com>
2025-05-11 20:53:10 +0300

    Update config.py

--- emilia 2.0/config.py
+++ emilia 2.0/config.py
@@ -5,55 +5,6 @@ CHANNEL_LINK = "https://t.me/Emilia_debag"
 USER_CATEGORIES = {
     "Безопасный контент 🔒": {
         "Подкатегории": {
-            "Геншин Импакт 🏮": ["Genshin_Impact"],
-            "Хонкай: Звёздный рельс 🌌": ["HonkaiStarRail"],
-            "Ре: Зеро ⏳": ["Re_Zero"],
-            "Няшки-неко 🐾": ["awwnime", "neko"],
-            "Аниме-арты 🎨": ["AnimeART"],
-            "Мемы про аниме 🤣": ["Animemes"]
-        },
-        "nsfw": False
-    },
-    "Взрослый контент 🔞": {
-        "Подкатегории": {
-            "Лоликон 👧": ["lolicon"],
-            "Юри 🌸": ["yuri"),
-            "Неко-эротика 🐾": ["neko_NSFW"],
-            "Трапы 🎎": ["traphentai"),
-            "Яой 💢": ["yaoi"),
-            "Экстремальные арты 🔥": ["HentaiHardcore")
-        },
-        "nsfw": True
-    }
-}
-
-SCROLLLER_MAPPING = {
-    # SFW
-    "Genshin_Impact": "GenshinImpact",
-    "HonkaiStarRail": "HonkaiStarRail",
-    "Re_Zero": "Re_Zero",
-    "awwnime": "awwnime",
-    "neko": "neko",
-    "AnimeART": "AnimeART",
-    "Animemes": "goodanimemes",
-    
-    # NSFW
-    "lolicon": "loliconnsfw",
-    "yuri": "yurihentai",
-    "neko_NSFW": "nekonsfw",
-    "traphentai": "traphentai",
-    "yaoi": "yaoi",
-    "HentaiHardcore": "HentaiHardcore"
-}
-
-
-BOT_TOKEN = "ВАШ_ТОКЕН"
-CHANNEL_ID = "@ВАШ_КАНАЛ"
-CHANNEL_LINK = "https://t.me/ВАШ_КАНАЛ"
-
-USER_CATEGORIES = {
-    "Безопасный контент 🔒": {
-        "Подкатегории": {
             "Миловидные персонажи 🐾": ["awwnime", "Moescape"],
             "Обои для рабочего стола 🖼️": ["AnimeWallpaper"]
         },
